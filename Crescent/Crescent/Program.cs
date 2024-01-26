42783d4e2766ba0cb8807123f6edd9baba436338
AbidAli <101678943+abidxcrescent@users.noreply.github.com>
2024-01-25 22:04:30 -0700

    Add files via upload
    Initial Commit

---
+++ Crescent/Crescent/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crescent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 25;
+            double salary = 5.3;
+            bool isMale = true;
+            string name = "Abid";
+
+            Console.WriteLine("Abid");
+            Console.ReadLine();
+
+            int[] luckyNumbers = new int[3] {1,2,3};
+
+            List<String> names = new List<string>();
+            names.Add("Abid");
+
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+
+            HashSet<int> list = new HashSet<int>();
+        }
+    }
+}
